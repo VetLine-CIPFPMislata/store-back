ca01936d1b3d375a42d5028cbf2e7ef3397657e3
Adrian <ositiredulo@gmail.com>
2026-02-18 11:59:02 +0100

    feat(VetLine/sprint4#2)do orders

--- src/main/java/org/example/storeback/domain/service/impl/PaymentServiceImpl.java
+++ src/main/java/org/example/storeback/domain/service/impl/PaymentServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
-@Service
+
 public class PaymentServiceImpl implements PaymentService {
 
     private final BankPaymentService bankPaymentService;
